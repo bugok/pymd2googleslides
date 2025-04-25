b1061a154c535f935b37af8c9d5fb0e678ca09f9
Noam Lerner <noam.lerner@gmail.com>
2025-04-25 14:00:28 +0300

    After ruff formatting

--- pymd2googleslides/main.py
+++ pymd2googleslides/main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import click
 
+
 @click.group
 def cli():
     pass
 
+
 def main():
     print("Hello from pymd2googleslides")
+
 
 if __name__ == "__main__":
     cli()
--- tests/main.py
+++ tests/main.py
@@ -4,9 +4,9 @@ import testslide
 from click.testing import CliRunner
 from main import cli
 
+
 class MainTest(testslide.TestCase):
     def test_sanity(self):
         runner = CliRunner()
         result = runner.invoke(cli, ["--help"])
         self.assertEquals(0, result.exit_code)
-
