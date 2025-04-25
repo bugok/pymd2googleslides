bc57d249477e9ea24f642abe720655ee5f363bc2
Noam Lerner <noam.lerner@gmail.com>
2025-04-25 14:04:08 +0300

    Add github actions

--- tests/main.py
+++ tests/main.py
@@ -2,11 +2,11 @@
 
 import testslide
 from click.testing import CliRunner
-from main import cli
+from pymd2googleslides.main import cli
 
 
 class MainTest(testslide.TestCase):
     def test_sanity(self):
         runner = CliRunner()
         result = runner.invoke(cli, ["--help"])
-        self.assertEquals(0, result.exit_code)
+        self.assertEqual(0, result.exit_code)
